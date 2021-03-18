5b21743af833ac37b8e5c67cf61199f2f25d173c
Harry Stead <harrystead9@gmail.com>
2021-03-18 09:55:42 +0000

    server added

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}!`);
+});
